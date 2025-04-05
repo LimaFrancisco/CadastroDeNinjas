7d2c72aac3f11a5d16115c910477487989a4fa22
LimaFrancisco <francisco1111.fl@protonmail.com>
2025-04-05 16:09:34 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,10 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missao")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissaoModel {
 
     @Id
@@ -17,41 +24,4 @@ public class MissaoModel {
     // Uma missão vai ter vários ninjas
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninjas;
-
-    public MissaoModel() {
-    }
-
-    public MissaoModel(List<NinjaModel>  ninjas, RankMissao dificuldade, String nome) {
-        this.ninjas = ninjas;
-        this.dificuldade = dificuldade;
-        this.nome = nome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public RankMissao getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(RankMissao dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
-
-    public Long getId(){
-        return this.id;
-    }
 }
