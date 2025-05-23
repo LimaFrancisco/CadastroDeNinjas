620991778cd4a269ef44adbdb03a853d89b56de2
LimaFrancisco <francisco1111.fl@protonmail.com>
2025-05-23 14:04:28 -0300

    feat: Cria as rotas do controller com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missao/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missao/MissaoController.java
@@ -1,4 +1,33 @@
 package dev.java10x.CadastroDeNinjas.Missao;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missao")
 public class MissaoController {
+
+    // POST -- Mandar uma requisição para criar uma missão
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missão criada";
+    }
+
+
+    // GET -- Mandar uma requisição para listar as missões
+    @GetMapping("/ler")
+    public String listarMissoes(){
+        return "Missões listadas";
+    }
+
+    // PUT -- Mandar uma requisição para atualizar as missões
+    @PutMapping("/alterar")
+    public String alterarMissaoPorId(){
+        return "Missao alterada com sucesso";
+    }
+
+    // DELETE -- Mandar uma requisição para deletar uma missão
+    @DeleteMapping("/delete")
+    public String deletarMissao(){
+        return "Missao deletada com sucesso";
+    }
 }
